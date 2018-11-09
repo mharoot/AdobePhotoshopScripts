e6cd3cdb2aef5957c1acd4cba3a2787d88a35702
mharoot <michaelharootoonyan@yahoo.com>
2018-11-09 11:02:18 -0800

    finished but make note in the future the requirements were not clear for this one

--- adobe_scripts/production/087_expect_UPS_label_virtual.jsx
+++ adobe_scripts/production/087_expect_UPS_label_virtual.jsx
@@ -1,1 +1,38 @@
-// 087_expect_UPS_label_virtual.jsx
+#include "json2.js" 
+// global variables must be declared before the start
+var dir = "/Users/gpcolor/Desktop/digitaldecor-backend/bin/virtual_UPS_label/"; 
+
+(function start(){
+    var order = loadJson();
+    open_UPS_label(order);
+    
+})();
+
+function loadJson() {
+    var script = new File($.fileName).toString();
+    var json_path = script.split('087_expect_UPS_label_virtual.jsx').shift() + 'order.json'
+    var jsonFile = new File(json_path);
+    jsonFile.open('r');
+    var str = jsonFile.read();
+    jsonFile.close();
+    return JSON.parse(str);
+}
+
+
+/**
+ * Opens the UPS label gif.
+ * @param {JSON} order if file is not found paste generic UPS label 'ups_label_virtual.gif'
+ * @return {void} 
+ */
+function open_UPS_label(order)
+{
+    var labelFile = "virtual_UPS_" + order["_id"] + "_virtual.gif";
+    try {
+        file = new File(dir+labelFile); 
+        app.open(file);
+    } catch (e)
+    {
+        file = new File(dir+"ups_label_virtual.gif");
+        app.open(file);
+    }
+}
